c6672f68699d74864ba2c5508dc8bfc3e68e59d2
Iman Heizer Pua <imanheizer@gmail.com>
2021-02-13 19:02:28 +0800

    masked image to json script for cityscapes dataset added

--- image_to_coco.py
+++ image_to_coco.py
@@ -75,79 +75,37 @@ def create_sub_mask_annotation(sub_mask, image_id, category_id, annotation_id, i
 
 if __name__ == '__main__':
 
-    labels = [
-    #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
-    Label(  'unlabeled'            ,  0 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'ego vehicle'          ,  1 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'rectification border' ,  2 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'out of roi'           ,  3 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'static'               ,  4 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'dynamic'              ,  5 ,      255 , 'void'            , 0       , False        , True         , (111, 74,  0) ),
-    Label(  'ground'               ,  6 ,      255 , 'void'            , 0       , False        , True         , ( 81,  0, 81) ),
-    Label(  'road'                 ,  7 ,        0 , 'flat'            , 1       , False        , False        , (128, 64,128) ),
-    Label(  'sidewalk'             ,  8 ,        1 , 'flat'            , 1       , False        , False        , (244, 35,232) ),
-    Label(  'parking'              ,  9 ,      255 , 'flat'            , 1       , False        , True         , (250,170,160) ),
-    Label(  'rail track'           , 10 ,      255 , 'flat'            , 1       , False        , True         , (230,150,140) ),
-    Label(  'building'             , 11 ,        2 , 'construction'    , 2       , False        , False        , ( 70, 70, 70) ),
-    Label(  'wall'                 , 12 ,        3 , 'construction'    , 2       , False        , False        , (102,102,156) ),
-    Label(  'fence'                , 13 ,        4 , 'construction'    , 2       , False        , False        , (190,153,153) ),
-    Label(  'guard rail'           , 14 ,      255 , 'construction'    , 2       , False        , True         , (180,165,180) ),
-    Label(  'bridge'               , 15 ,      255 , 'construction'    , 2       , False        , True         , (150,100,100) ),
-    Label(  'tunnel'               , 16 ,      255 , 'construction'    , 2       , False        , True         , (150,120, 90) ),
-    Label(  'pole'                 , 17 ,        5 , 'object'          , 3       , False        , False        , (153,153,153) ),
-    Label(  'polegroup'            , 18 ,      255 , 'object'          , 3       , False        , True         , (153,153,153) ),
-    Label(  'traffic light'        , 19 ,        6 , 'object'          , 3       , False        , False        , (250,170, 30) ),
-    Label(  'traffic sign'         , 20 ,        7 , 'object'          , 3       , False        , False        , (220,220,  0) ),
-    Label(  'vegetation'           , 21 ,        8 , 'nature'          , 4       , False        , False        , (107,142, 35) ),
-    Label(  'terrain'              , 22 ,        9 , 'nature'          , 4       , False        , False        , (152,251,152) ),
-    Label(  'sky'                  , 23 ,       10 , 'sky'             , 5       , False        , False        , ( 70,130,180) ),
-    Label(  'person'               , 24 ,       11 , 'human'           , 6       , True         , False        , (220, 20, 60) ),
-    Label(  'rider'                , 25 ,       12 , 'human'           , 6       , True         , False        , (255,  0,  0) ),
-    Label(  'car'                  , 26 ,       13 , 'vehicle'         , 7       , True         , False        , (  0,  0,142) ),
-    Label(  'truck'                , 27 ,       14 , 'vehicle'         , 7       , True         , False        , (  0,  0, 70) ),
-    Label(  'bus'                  , 28 ,       15 , 'vehicle'         , 7       , True         , False        , (  0, 60,100) ),
-    Label(  'caravan'              , 29 ,      255 , 'vehicle'         , 7       , True         , True         , (  0,  0, 90) ),
-    Label(  'trailer'              , 30 ,      255 , 'vehicle'         , 7       , True         , True         , (  0,  0,110) ),
-    Label(  'train'                , 31 ,       16 , 'vehicle'         , 7       , True         , False        , (  0, 80,100) ),
-    Label(  'motorcycle'           , 32 ,       17 , 'vehicle'         , 7       , True         , False        , (  0,  0,230) ),
-    Label(  'bicycle'              , 33 ,       18 , 'vehicle'         , 7       , True         , False        , (119, 11, 32) ),
-    Label(  'license plate'        , -1 ,       -1 , 'vehicle'         , 7       , False        , True         , (  0,  0,142) ),
-]
+    #cityscapes colour to ID
+    color2id = {'(0, 0, 0)': 4, '(111, 74, 0)': 5, '(81, 0, 81)': 6, '(128, 64, 128)': 7, '(244, 35, 232)': 8, '(250, 170, 160)': 9, '(230, 150, 140)': 10, 
+    '(70, 70, 70)': 11, '(102, 102, 156)': 12, '(190, 153, 153)': 13, '(180, 165, 180)': 14, '(150, 100, 100)': 15, '(150, 120, 90)': 16, '(153, 153, 153)': 18, 
+    '(250, 170, 30)': 19, '(220, 220, 0)': 20, '(107, 142, 35)': 21, '(152, 251, 152)': 22, '(70, 130, 180)': 23, '(220, 20, 60)': 24, '(255, 0, 0)': 25, 
+    '(0, 0, 142)': -1, '(0, 0, 70)': 27, '(0, 60, 100)': 28, '(0, 0, 90)': 29, '(0, 0, 110)': 30, '(0, 80, 100)': 31, '(0, 0, 230)': 32, '(119, 11, 32)': 33}
 
-    plant_book_mask_image = Image.open('/path/to/images/plant_book_mask.png')
-    bottle_book_mask_image = Image.open('/path/to/images/bottle_book_mask.png')
-
-    mask_images = [plant_book_mask_image, bottle_book_mask_image]
-
-    # Define which colors match which categories in the images
-    houseplant_id, book_id, bottle_id, lamp_id = [1, 2, 3, 4]
-    category_ids = {
-        1: {
-            '(0, 255, 0)': houseplant_id,
-            '(0, 0, 255)': book_id,
-        },
-        2: {
-            '(255, 255, 0)': bottle_id,
-            '(255, 0, 128)': book_id,
-            '(255, 100, 0)': lamp_id,
-        }
-    }
-
-    is_crowd = 0
+    test = Image.open('test.png')
+    mask_images = [test]
 
     # These ids will be automatically increased as we go
     annotation_id = 1
     image_id = 1
+    is_crowd = 0
+
+    # root = "/root/directory/here"
+
+    # for image in root:
+    #     image__ = Image.open(image)
+    #     mask_images.append(image__)
 
     # Create the annotations
     annotations = []
     for mask_image in mask_images:
         sub_masks = create_sub_masks(mask_image)
         for color, sub_mask in sub_masks.items():
-            category_id = category_ids[image_id][color]
+            category_id = color2id[color]
             annotation = create_sub_mask_annotation(sub_mask, image_id, category_id, annotation_id, is_crowd)
             annotations.append(annotation)
             annotation_id += 1
         image_id += 1
 
-    print(json.dumps(annotations))
+
+    with open('data.json', 'w') as data:
+        json.dump(annotations, data)
